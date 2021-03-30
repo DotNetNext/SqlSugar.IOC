d7c5a30e996ed8378f066847575a208918028ab3
SUNKAIXUAN <610262374@qq.com>
2021-03-30 16:05:51 +0800

    添加控制台注入

---
+++ Core/SqlSugar.IOC/SqlSugar.IOC/IocServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlSugar.IOC
+{
+    public class SugarIocServices
+    {
+        public static void AddSqlSugar(IocConfig ioc)
+        {
+            SugarServiceCollectionExtensions.configs.Add(ioc);
+        }
+        public static void AddSqlSugar(List<IocConfig> iocList)
+        {
+            SugarServiceCollectionExtensions.configs.AddRange(iocList);
+        }
+    }
+}
